1d9cbdc21667e2d04d318da97018bae55d29772f
Schuyler <schuyler.lindberg@ubc.ca>
2017-12-08 17:36:38 -0800

    fft fun. don't use the filteredArr.

--- src/listener.js
+++ src/listener.js
@@ -1,16 +1,13 @@
 const listener = {}
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
+  source,
+  stream;
+
+let analyser = audioCtx.createAnalyser();
+analyser.smoothingTimeConstant = 0.85;
 
 listener.init = () => {
   console.log('listen!');
-  let audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
-      source,
-      stream;
-
-  let analyser = audioCtx.createAnalyser();
-  analyser.minDecibels = -90;
-  analyser.maxDecibels = -10;
-  analyser.smoothingTimeConstant = 0.85;
-
   if (navigator.mediaDevices.getUserMedia) {
     console.log('getUserMedia supported.');
     setTimeout(function(){
@@ -23,9 +20,7 @@ listener.init = () => {
         function (stream) {
           source = audioCtx.createMediaStreamSource(stream);
           source.connect(analyser);
-          analyser.connect(distortion);
-          // visualize();
-          console.log(stream);
+          console.log("connected to:", stream);
         }).catch(
         // Error callback
         function (err) {
@@ -36,5 +31,39 @@ listener.init = () => {
     console.log('getUserMedia not supported on your browser!');
   }
 }
+
+listener.triBand = () => {
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  analyser.getByteFrequencyData(dataArray);
+
+  let filteredArr = dataArray.filter(function(val){ return val !== 0; })
+  let thirds = Math.floor(filteredArr.length / 3);
+  let bands = {
+    low : normalize(filteredArr.slice(0, thirds)),
+    mid : normalize(filteredArr.slice(thirds, thirds*2)),
+    high : normalize(filteredArr.slice(thirds*2, filteredArr.length)),
+  };
+  function normalize(bufferArr){
+    if (bufferArr.length < 1) { return 0; }
+    let val = 0;
+    for (const n of bufferArr) {
+      if(!isNaN(n)){
+        val += n;
+      }
+    }
+    return val / bufferArr.length;
+  }
+  console.log(filteredArr.length, normalize(dataArray))
+  console.log(bands);
+}
+
+// function
+
+setInterval(function(){
+  listener.triBand();
+},300)
 
 export default listener
